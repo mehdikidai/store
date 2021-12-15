2dbf9b4521d1d66e16dae2042e7019b36b9bc046
mehdi_kidai <mehdikidai@gmail.com>
2021-12-16 00:39:52 +0100

    75239cb1-8f81-4754-ab07-23ff03f22b36

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
 
-    startDate: new Date("2020-01-20"),
-    endDate: new Date("2021-12-01"),
-    numberOfDates: 400,
-    
+    startDate: new Date("2021-12-01"),
+    endDate: new Date("2021-12-20"),
+    numberOfDates: 10,
+
 };
