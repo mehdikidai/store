cc4d348fa0cc3474921ff21646c0f898a66df566
mehdi_kidai <mehdikidai@gmail.com>
2023-02-21 22:02:19 +0100

    444aa3b3-01c8-4d8c-aac0-6703b3cbcd5e

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
 
-    startDate: new Date("2023-01-01"),
-    endDate: new Date("2023-02-15"),
+    startDate: new Date("2023-02-01"),
+    endDate: new Date("2023-03-15"),
     numberOfDates: 10,
 
 };
